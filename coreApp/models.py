c7a5f6862797ab2eb106dc5dc02b76551ed469b5
MoltenWolfCub <oliver@tomlins.org.uk>
2023-01-22 14:29:13 +0000

    gave a balance an owner

--- coreApp/models.py
+++ coreApp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class Balance(models.Model):
     """The money stored in a user's account."""
-    # owner = models.OneToOneField(User)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     amount = models.IntegerField()
 
     def __str__(self) -> str:
