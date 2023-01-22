68a08507381706f44ef86ce263bbaebb1da19ced
MoltenWolfCub <oliver@tomlins.org.uk>
2023-01-22 08:34:44 +0000

    created balance model

--- coreApp/models.py
+++ coreApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+class Balance(models.Model):
+    """The money stored in a user's account."""
+    # owner = models.OneToOneField(User)
+    amount = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.amount
