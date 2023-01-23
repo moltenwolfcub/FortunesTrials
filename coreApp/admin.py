6f09c65235258520d851d1ed67a9408d08e61627
MoltenWolfCub <oliver@tomlins.org.uk>
2023-01-23 16:41:22 +0000

    renamed balance model to a more appropriate name
    balance -> bankAccount
    amount -> balance
    
    the balance is the amount of money not the account

--- coreApp/admin.py
+++ coreApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Balance
+from .models import BankAccount
 
-admin.site.register(Balance)
+admin.site.register(BankAccount)
--- coreApp/models.py
+++ coreApp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Balance(models.Model):
+class BankAccount(models.Model):
     """The money stored in a user's account."""
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    amount = models.IntegerField()
+    balance = models.IntegerField()
 
     def __str__(self) -> str:
-        return self.amount
+        return self.balance
