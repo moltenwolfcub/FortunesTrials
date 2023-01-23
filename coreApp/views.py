c49136248d889086aa81f10c3179630d3e0ae8b0
MoltenWolfCub <oliver@tomlins.org.uk>
2023-01-23 17:56:17 +0000

    redirected unsigned-in users to login page
    if you are trying to access account specific information
    (like the bank account) you need to be logged in so you get
    redirected to that page when trying to view

--- coreApp/views.py
+++ coreApp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .models import BankAccount
 
@@ -8,6 +9,7 @@ def index(request: HttpRequest):
     """Homepage"""
     return render(request, "coreApp/index.html")
 
+@login_required
 def bank(request: HttpRequest):
     """The place to view your bank account and it's balance."""
     account: BankAccount = request.user.bankaccount
