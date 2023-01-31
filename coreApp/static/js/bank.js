5f5dacc85d4ef7ed7912725693f37a521479a49f
MoltenWolfCub <oliver@tomlins.org.uk>
2023-01-31 20:32:59 +0000

    fixed timezone offset issue with daily countdown

--- coreApp/static/js/bank.js
+++ coreApp/static/js/bank.js
@@ -3,10 +3,33 @@
  * @param {string} lastDaily formatted in iso
  */
 function updateDailyCooldown(lastDaily) {
-    let datedLast = new Date(lastDaily);
-    let since = new Date(new Date() - datedLast);
-    let cooldown = datedLast.getTime() + 86_400_000 - since
-    let formattedCooldown = new Date(cooldown).toLocaleTimeString()
+    let msLast = +new Date(lastDaily);
+    let msSince = Date.now()-msLast;
+    let cooldownLength = 86_400_000;
+    let timeLeft = cooldownLength-msSince;
+    let fmtTimeLeft = formatMs(timeLeft);
 
-    document.getElementById("dailyCooldown").innerHTML = formattedCooldown;
+    document.getElementById("dailyCooldown").innerHTML = fmtTimeLeft;
+}
+
+function formatMs(s) {
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    var returnStr = secs;
+    if (mins > 0) {
+        returnStr = mins + ':' + returnStr;
+    }
+    if (hrs > 0) {
+        if (mins <= 0) {
+            returnStr = mins + ':' + returnStr;
+        }
+        returnStr = hrs + ':' + returnStr;
+    }
+
+    return returnStr;
 }
